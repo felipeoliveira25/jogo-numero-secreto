9e7fe4b487b185a98e98c825819633d128c0e78e
felipeoliveira25 <felipeoferreira.25@gmail.com>
2024-02-18 22:14:22 -0300

    adicionando as funcionalidades para escolher o menor e o maior número, e o botão para clicar e falar

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,5 +1,5 @@
-const menorValor = 1
-const maiorValor = 1000
+let menorValor = 2
+let maiorValor = 1000
 const numeroSecreto = gerarNumeroAleatorio()
 
 function gerarNumeroAleatorio() {
@@ -13,3 +13,31 @@ elementoMenorValor.innerHTML = menorValor
 
 const elementoMaiorValor = document.getElementById('maior-valor')
 elementoMaiorValor.innerHTML = maiorValor
+
+
+const valorMenorInput = document.getElementById('input-numero-menor')
+const valorMaiorInput = document.getElementById('input-numero-maior')
+const botaoMudarParametro = document.getElementById('btn-mudar-parametro')
+const formMudarParametro = document.getElementById('form-mudar-numero')
+botaoMudarParametro.addEventListener('click', () => {
+    if(formMudarParametro.classList.contains('hidden')){
+        formMudarParametro.classList.remove('hidden');
+        botaoMudarParametro.textContent = "Cancelar"
+    }else{
+        formMudarParametro.classList.add('hidden')
+        botaoMudarParametro.textContent = "Mudar Parâmetros"
+    } 
+})
+
+formMudarParametro.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    elementoMaiorValor.innerHTML = valorMaiorInput.value
+    elementoMenorValor.innerHTML = valorMenorInput.value
+    menorValor = valorMenorInput.value
+    maiorValor = valorMaiorInput.value
+    formMudarParametro.classList.add('hidden')
+        botaoMudarParametro.textContent = "Mudar Parâmetros"
+
+
+})
+
