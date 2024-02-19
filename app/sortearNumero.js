c0fd9f1f07989812d702dfe80c753b808963c5cf
felipeoliveira25 <felipeoferreira.25@gmail.com>
2024-02-18 23:14:23 -0300

    commitando alterações no processo de mudança dos parâmetros

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,24 +1,50 @@
-let menorValor = 2
+const valorMenorInput = document.getElementById('input-numero-menor')
+const valorMaiorInput = document.getElementById('input-numero-maior')
+let menorValor = 1
 let maiorValor = 1000
-const numeroSecreto = gerarNumeroAleatorio()
+let numeroSecreto = gerarNumeroAleatorio(maiorValor)
+const elementoMaiorValor = document.getElementById('maior-valor')
+const elementoMenorValor = document.getElementById('menor-valor')
+const botaoMudarParametro = document.getElementById('btn-mudar-parametro')
+const formMudarParametro = document.getElementById('form-mudar-numero')
 
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * maiorValor + 1)
+formMudarParametro.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    elementoMaiorValor.innerHTML = valorMaiorInput.value
+    elementoMenorValor.innerHTML = valorMenorInput.value
+    menorValor = valorMenorInput.value
+    maiorValor = valorMaiorInput.value
+    formMudarParametro.classList.add('hidden')
+    botaoMudarParametro.textContent = "Mudar Parâmetros";
+    alert("Número sorteado!!!! Vamos jogar!")
+    numeroSecreto = gerarNumeroAleatorio(maiorValor)
+    console.log(numeroSecreto)
+    
+    
+    
+})
+
+
+
+
+
+
+function gerarNumeroAleatorio(valor) {
+    return parseInt(Math.random() * valor + 1)
 }
 
 console.log('Número Secreto:', numeroSecreto)
 
-const elementoMenorValor = document.getElementById('menor-valor')
-elementoMenorValor.innerHTML = menorValor
-
-const elementoMaiorValor = document.getElementById('maior-valor')
-elementoMaiorValor.innerHTML = maiorValor
 
 
-const valorMenorInput = document.getElementById('input-numero-menor')
-const valorMaiorInput = document.getElementById('input-numero-maior')
-const botaoMudarParametro = document.getElementById('btn-mudar-parametro')
-const formMudarParametro = document.getElementById('form-mudar-numero')
+
+
+
+
+
+
+//mostrar ou tirar o form que muda os parâmetros
+
 botaoMudarParametro.addEventListener('click', () => {
     if(formMudarParametro.classList.contains('hidden')){
         formMudarParametro.classList.remove('hidden');
@@ -28,16 +54,4 @@ botaoMudarParametro.addEventListener('click', () => {
         botaoMudarParametro.textContent = "Mudar Parâmetros"
     } 
 })
-
-formMudarParametro.addEventListener('submit', (e)=> {
-    e.preventDefault();
-    elementoMaiorValor.innerHTML = valorMaiorInput.value
-    elementoMenorValor.innerHTML = valorMenorInput.value
-    menorValor = valorMenorInput.value
-    maiorValor = valorMaiorInput.value
-    formMudarParametro.classList.add('hidden')
-        botaoMudarParametro.textContent = "Mudar Parâmetros"
-
-
-})
-
+///////////////////////////////////////////////////////////////////////////////////
